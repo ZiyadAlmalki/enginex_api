192a8395c63040f2c6524c92fa5309037b3b6f35
Daniel Tashman <86805843+brookehorizon@users.noreply.github.com>
2022-09-22 21:15:06 +0300

    Update settings.py

--- enginex_api/settings.py
+++ enginex_api/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@=!cmuya7y2uz%zr#xuxv1co7uf3s%3^43lht$f!#)+#$&%+_*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
